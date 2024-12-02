834fb13f8b1ae424ee7b8e667e34d21ca3fb68f6
Syed Ateebul Islam <32341313+syedateebulislam@users.noreply.github.com>
2024-12-02 14:06:49 +0530

    latest commit

--- src/main/java/org/example/design/Design_Pattern/Builder_Pattern/Process.java
+++ src/main/java/org/example/design/Design_Pattern/Builder_Pattern/Process.java
@@ -2,7 +2,12 @@ package org.example.design.Design_Pattern.Builder_Pattern;
 
 public class Process {
 	public static void main(String[] args) {
-		Vehicle v1=new Vehicle.Builder("UP60 V 0123", 72244).mfgBy("Honda").modelName("Activa").Build();
+		Vehicle v1=new
+				Vehicle	//Outer vehicle class
+				.Builder("UP60 V 0123")	//inner Builder class
+				.modelType("Scooty")			//optional field 1 setter
+				.modelName("Activa")			//optional field 2 setter
+				.Build();	//Inner Build const called, which finally calls outer class object constructor
 		System.out.println(v1.toString());
 	}
 }
--- src/main/java/org/example/design/Design_Pattern/Builder_Pattern/Vehicle.java
+++ src/main/java/org/example/design/Design_Pattern/Builder_Pattern/Vehicle.java
@@ -1,72 +1,71 @@
 package org.example.design.Design_Pattern.Builder_Pattern;
 
+
+/*
+1-private constructor - (abnormal stuff 1 : Take inner class Builder in inp param)
+2-Nested Builder Class - (abnormal stuff 2 : Create inner class Builder with same fields)
+3-Normal Builder const - (abnormal stuff 3 : public Builder const with mandatory fields in inp param)
+4-All optional value setters - (abnormal stuff 4 : With return type as inner Builder class)
+5-Build constructor - (abnormal stuff 5 : Outer Vehicle class Build const to return Vehicle of current instance type)
+*/
+
+
 public class Vehicle {
 	
-	private String numPlate;//mandatory
-	private int price;//mandatory
-	private String mfgBy;
+	private String numPlate;	//user defined mandatory field
 	private String modelType;
 	private String modelName;
-	private int mileage;
 	
-	//1-private constructor
+	//1-private constructor - (abnormal stuff 1 : Take inner class Builder in inp param)
 	private Vehicle(Builder b) {
+		System.out.println("Vehicle constructor called");
 		this.numPlate=b.numPlate;
-		this.price=b.price;
-		this.mfgBy=b.mfgBy;
 		this.modelType=b.modelType;
 		this.modelName=b.modelName;
-		this.mileage=b.mileage;
-	}
-	
-	
-	
-	@Override
-	public String toString() {
-		return "Vehicle [numPlate=" + numPlate + ", price=" + price + ", mfgBy=" + mfgBy + ", modelType=" + modelType
-				+ ", modelName=" + modelName + ", mileage=" + mileage + "]";
 	}
 
-
-	//2-Nested Builder Class
+	//2-Nested Builder Class - (abnormal stuff 2 : Create inner class Builder with same fields)
 	public static class Builder{
 
 		private String numPlate;//mandatory
-		private int price;//mandatory
-		private String mfgBy;
 		private String modelType;
 		private String modelName;
-		private int mileage;
+
 		
-		//3-Builder constructor with mandatory parameters
-		public Builder(String numplate,int price) {
+		//3-Normal Builder const - (abnormal stuff 3 : public Builder const with mandatory fields in inp param)
+		public Builder(String numplate) {
+			System.out.println("Builder constructor called");
 			this.numPlate=numplate;
-			this.price=price;
 		}
 		
-		//4-Build constructor
-		public Vehicle Build() {
-			return new Vehicle(this);
-		}
-		
-		//5-All other optional value setters
-		public Builder mfgBy(String value) {
-			this.mfgBy=value;
-			return this;
-		}
+		//4-All optional value setters - (abnormal stuff 4 : With return type as inner Builder class)
 		public Builder modelType(String value) {
+			System.out.println("modelType setter called");
 			this.modelType=value;
 			return this;
 		}
-		
+
 		public Builder modelName(String value) {
+			System.out.println("modelName setter called");
 			this.modelName=value;
 			return this;
 		}
 
-		public Builder mileage(int value) {
-			this.mileage=value;
-			return this;
+		//5-Build constructor - (abnormal stuff 5 : Outer Vehicle class Build const to return Vehicle of current instance type)
+		public Vehicle Build() {
+			System.out.println("Build constructor to call Vehicle Const called");
+			return new Vehicle(this);
 		}
+
 	}
+
+	@Override
+	public String toString() {
+		return "\nVehicle{" +
+				"numPlate='" + numPlate + '\'' +
+				", modelType='" + modelType + '\'' +
+				", modelName='" + modelName + '\'' +
+				'}';
+	}
+
 }
